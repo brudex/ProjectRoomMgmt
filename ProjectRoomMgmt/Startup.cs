a9a5c3f2e4d657de2a0197bf31a4d9dc408938a6
nana safo <brudexgh@gmail.com>
2019-01-11 00:06:06 +0000

    initial commit

---
+++ ProjectRoomMgmt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectRoomMgmt.Startup))]
+namespace ProjectRoomMgmt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
